8d60243ee7034234c4ae51ff084d43af0724f3a0
TanveerShahriar <tanveershahriara@gmail.com>
2022-05-24 00:16:35 +0600

    footer added

--- src/App.js
+++ src/App.js
@@ -4,24 +4,28 @@ import CustomTitle from './Pages/Shared/CustomTitle'
 import Home from './Pages/Home/Home';
 import Login from './Pages/Login/Login';
 import Navbar from './Pages/Shared/Navbar';
+import Footer from './Pages/Shared/Footer';
 
 function App() {
   return (
-    <div className="App">
-      <Navbar></Navbar>
-      <Routes>
-        <Route path='/' element={
-          <CustomTitle title={"Home"}>
-            <Home />
-          </CustomTitle>
-        }></Route>
+    <div className="App flex flex-col h-screen">
+      <div className='mb-auto'>
+        <Navbar></Navbar>
+        <Routes>
+          <Route path='/' element={
+            <CustomTitle title={"Home"}>
+              <Home />
+            </CustomTitle>
+          }></Route>
 
-        <Route path='/login' element={
-          <CustomTitle title={"Login"}>
-            <Login />
-          </CustomTitle>
-        }></Route>
-      </Routes>
+          <Route path='/login' element={
+            <CustomTitle title={"Login"}>
+              <Login />
+            </CustomTitle>
+          }></Route>
+        </Routes>
+      </div>
+      <Footer></Footer>
     </div>
   );
 }
