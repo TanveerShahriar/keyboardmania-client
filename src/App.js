af08f0712ddc7f57fd87f22d041d55cc41b4ef36
TanveerShahriar <tanveershahriara@gmail.com>
2022-05-23 20:34:54 +0600

    navbar created

--- src/App.js
+++ src/App.js
@@ -1,9 +1,27 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import CustomTitle from './Pages/Shared/CustomTitle'
+import Home from './Pages/Home/Home';
+import Login from './Pages/Login/Login';
+import Navbar from './Pages/Shared/Navbar';
 
 function App() {
   return (
     <div className="App">
-      
+      <Navbar></Navbar>
+      <Routes>
+        <Route path='/' element={
+          <CustomTitle title={"Home"}>
+            <Home />
+          </CustomTitle>
+        }></Route>
+
+        <Route path='/login' element={
+          <CustomTitle title={"Login"}>
+            <Login />
+          </CustomTitle>
+        }></Route>
+      </Routes>
     </div>
   );
 }
