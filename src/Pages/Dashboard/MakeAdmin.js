b74c690b024be4e782561610dd33624f6eac9d17
TanveerShahriar <tanveershahriara@gmail.com>
2022-05-25 20:09:00 +0600

    user remove created

--- src/Pages/Dashboard/MakeAdmin.js
+++ src/Pages/Dashboard/MakeAdmin.js
@@ -1,11 +1,94 @@
+import { signOut } from 'firebase/auth';
 import React from 'react';
+import { useQuery } from 'react-query';
+import { toast } from 'react-toastify';
+import Loading from '../Shared/Loading';
+import auth from '../../firebase.init';
 
-const MakeAdmin = () => {
+const Users = () => {
+    const { data: users, isLoading, refetch } = useQuery('users', () => fetch(`${process.env.REACT_APP_SERVER_LINK}/user`, {
+        method: 'GET',
+        headers: {
+            authorization: `Bearer ${localStorage.getItem('accessToken')}`
+        }
+    }).then(res => res.json()));
+
+    if (isLoading) {
+        return <Loading></Loading>
+    }
+
+    const makeAdmin = (email) => {
+        fetch(`${process.env.REACT_APP_SERVER_LINK}/user/admin/${email}`, {
+            method: 'PUT',
+            headers: {
+                authorization: `Bearer ${localStorage.getItem('accessToken')}`
+            }
+        })
+            .then(res => {
+                if (res.status === 403) {
+                    toast.error('Failed to Make an admin');
+                }
+                return res.json()
+            })
+            .then(data => {
+                if (data.modifiedCount > 0) {
+                    refetch();
+                    toast.success(`Successfully made an admin`);
+                }
+
+            })
+    }
+
+    const deleteUser = (id) => {
+        fetch(`${process.env.REACT_APP_SERVER_LINK}/user/${id}`, {
+            method: 'DELETE',
+            headers: {
+                authorization: `Bearer ${localStorage.getItem('accessToken')}`
+            }
+        })
+            .then(res => {
+                if (res.status === 401 || res.status === 403) {
+                    signOut(auth);
+                    localStorage.removeItem('accessToken');
+                }
+                return res.json()
+            })
+            .then(data => {
+                refetch();
+                toast("User delete");
+            })
+    }
+
     return (
         <div>
-            make admin
+            <h2 className="text-2xl">All Users: {users.length}</h2>
+            <div className="overflow-x-auto">
+                <table className="table w-full">
+                    <thead>
+                        <tr>
+                            <th>No.</th>
+                            <th>Email</th>
+                            <th colSpan={2} className="text-center">Actions</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            users.map((user, index) => <tr key={index}>
+                                <th>{index + 1}</th>
+                                <td>{user.email}</td>
+                                <td>{user.role !== 'admin' ?
+                                    <button onClick={() => makeAdmin(user.email)} className="btn btn-xs">Make Admin</button>
+                                    :
+                                    <div className="badge badge-accent">Admin</div>}
+                                </td>
+                                <td><button className="btn btn-xs" onClick={()=>deleteUser(user._id)}>Remove User</button></td>
+                            </tr>)
+                        }
+                    </tbody>
+                </table>
+            </div>
         </div>
     );
 };
 
-export default MakeAdmin;
+export default Users;
