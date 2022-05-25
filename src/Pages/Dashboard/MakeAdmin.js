a026f2eda95a6cf8faed0e5f8bcde07b307e4ce9
TanveerShahriar <tanveershahriara@gmail.com>
2022-05-25 20:38:35 +0600

    manage product created

--- src/Pages/Dashboard/MakeAdmin.js
+++ src/Pages/Dashboard/MakeAdmin.js
@@ -4,6 +4,7 @@ import { useQuery } from 'react-query';
 import { toast } from 'react-toastify';
 import Loading from '../Shared/Loading';
 import auth from '../../firebase.init';
+import swal from 'sweetalert';
 
 const Users = () => {
     const { data: users, isLoading, refetch } = useQuery('users', () => fetch(`${process.env.REACT_APP_SERVER_LINK}/user`, {
@@ -40,28 +41,35 @@ const Users = () => {
     }
 
     const deleteUser = (id) => {
-        fetch(`${process.env.REACT_APP_SERVER_LINK}/user/${id}`, {
-            method: 'DELETE',
-            headers: {
-                authorization: `Bearer ${localStorage.getItem('accessToken')}`
-            }
+        swal("Are you sure you want to delete your comment?", {
+            buttons: ["No!", true],
         })
-            .then(res => {
-                if (res.status === 401 || res.status === 403) {
-                    signOut(auth);
-                    localStorage.removeItem('accessToken');
+            .then(proceed => {
+                if (proceed) {
+                    fetch(`${process.env.REACT_APP_SERVER_LINK}/user/${id}`, {
+                        method: 'DELETE',
+                        headers: {
+                            authorization: `Bearer ${localStorage.getItem('accessToken')}`
+                        }
+                    })
+                        .then(res => {
+                            if (res.status === 401 || res.status === 403) {
+                                signOut(auth);
+                                localStorage.removeItem('accessToken');
+                            }
+                            return res.json()
+                        })
+                        .then(data => {
+                            refetch();
+                            toast("User delete");
+                        })
                 }
-                return res.json()
-            })
-            .then(data => {
-                refetch();
-                toast("User delete");
             })
     }
 
     return (
-        <div>
-            <h2 className="text-2xl">All Users: {users.length}</h2>
+        <div className='container mx-auto'>
+            <h2 className="text-2xl font-bold my-4">All Users: {users.length}</h2>
             <div className="overflow-x-auto">
                 <table className="table w-full">
                     <thead>
@@ -81,7 +89,7 @@ const Users = () => {
                                     :
                                     <div className="badge badge-accent">Admin</div>}
                                 </td>
-                                <td><button className="btn btn-xs" onClick={()=>deleteUser(user._id)}>Remove User</button></td>
+                                <td><button className="btn btn-xs" onClick={() => deleteUser(user._id)}>Remove User</button></td>
                             </tr>)
                         }
                     </tbody>
--- src/Pages/Dashboard/ManageProducts.js
+++ src/Pages/Dashboard/ManageProducts.js
@@ -1,9 +1,71 @@
-import React from 'react';
+import { signOut } from 'firebase/auth';
+import React, { useEffect, useState } from 'react';
+import swal from 'sweetalert';
+import auth from '../../firebase.init';
 
 const ManageProducts = () => {
+    const [products, setProducts] = useState([]);
+
+    useEffect(() => {
+        fetch(`${process.env.REACT_APP_SERVER_LINK}/products`)
+            .then(res => res.json())
+            .then(data => setProducts(data));
+    }, []);
+
+    const handleDelete = id => {
+        swal("Are you sure you want to delete this?", {
+            buttons: ["No!", true],
+        })
+            .then(proceed => {
+                if (proceed) {
+                    const url = `${process.env.REACT_APP_SERVER_LINK}/product/${id}`;
+                    fetch(url, {
+                        method: 'DELETE',
+                        headers: {
+                            authorization: `Bearer ${localStorage.getItem('accessToken')}`
+                        }
+                    })
+                        .then(res => {
+                            if (res.status === 401 || res.status === 403) {
+                                signOut(auth);
+                                localStorage.removeItem('accessToken');
+                            }
+                            return res.json()
+                        })
+                        .then(data => {
+                            const remaining = products.filter(product => product._id !== id);
+                            setProducts(remaining);
+                        })
+                }
+            })
+    }
+
     return (
-        <div>
-            manage product
+        <div className='container mx-auto'>
+            <h1 className='text-2xl font-bold my-4'>Inventory</h1>
+            <div className='overflow-x-auto'>
+                <table className='table w-full'>
+                    <thead>
+                        <tr>
+                            <th>Name</th>
+                            <th>Quantity</th>
+                            <th>Price</th>
+                            <th>Delete</th>
+                            <th></th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            products.map(product => <tr key={product._id}>
+                                <td>{product.name}</td>
+                                <td>{product.quantity}</td>
+                                <td>{product.price}</td>
+                                <td><button className='btn btn-outline btn-secondary' onClick={() => handleDelete(product._id)}>X</button></td>
+                            </tr>)
+                        }
+                    </tbody>
+                </table>
+            </div>
         </div>
     );
 };
