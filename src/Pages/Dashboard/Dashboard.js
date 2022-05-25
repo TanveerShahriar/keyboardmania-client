b02f53dd094fd93cb047d1a34cccc28f8727a9ca
TanveerShahriar <tanveershahriara@gmail.com>
2022-05-25 13:04:40 +0600

    order delete created

--- src/Pages/Dashboard/Dashboard.js
+++ src/Pages/Dashboard/Dashboard.js
@@ -12,7 +12,7 @@ const Dashboard = () => {
         <div className="drawer drawer-mobile">
             <input id="dashboard-sidebar" type="checkbox" className="drawer-toggle" />
             <div className="drawer-content">
-                <h2 className='text-2xl font-bold text-purple-500'>Welcome to Dashboard {user.displayName}</h2>
+                <h2 className='text-2xl font-bold text-secondary my-8'>Welcome to Dashboard {user.displayName}</h2>
                 <Outlet></Outlet>
             </div>
             <div className="drawer-side">
--- src/Pages/Dashboard/MyOrders.js
+++ src/Pages/Dashboard/MyOrders.js
@@ -1,12 +1,15 @@
 import { signOut } from 'firebase/auth';
 import React, { useEffect, useState } from 'react';
 import { useAuthState } from 'react-firebase-hooks/auth';
-import { useNavigate } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
+import { toast } from 'react-toastify';
+import swal from 'sweetalert';
 import auth from '../../firebase.init';
 
 const MyOrders = () => {
     const [orders, setOrders] = useState([]);
     const [user] = useAuthState(auth);
+    const [refresh, setRefresh] = useState(true);
     const navigate = useNavigate();
 
     useEffect(() => {
@@ -28,11 +31,86 @@ const MyOrders = () => {
                     setOrders(data);
                 });
         }
-    }, [user, navigate])
+    }, [refresh, user, navigate]);
+
+    const handleDelete = id => {
+        swal("Are you sure you want to delete your comment?", {
+            buttons: ["No!", true],
+        })
+            .then(proceed => {
+                if (proceed) {
+                    fetch(`${process.env.REACT_APP_SERVER_LINK}/order/${id}`, {
+                        method: 'DELETE',
+                        headers: {
+                            'authorization': `Bearer ${localStorage.getItem('accessToken')}`
+                        }
+                    })
+                        .then(res => {
+                            if (res.status === 401 || res.status === 403) {
+                                signOut(auth);
+                                localStorage.removeItem('accessToken');
+                            }
+                            return res.json()
+                        })
+                        .then(data => {
+                            setRefresh(!refresh);
+                            toast("Order Deleted")
+                        });
+                }
+            })
+    }
 
     return (
-        <div>
-            order {orders.length}
+        <div className='container mx-auto'>
+            <h2 className='font-bold text-xl my-4'>My Orders: {orders.length}</h2>
+            <div className="overflow-x-auto">
+                <table className="table w-full">
+                    <thead>
+                        <tr>
+                            <th>No.</th>
+                            <th>Name</th>
+                            <th>Address</th>
+                            <th>Price</th>
+                            <th colSpan={2} className="text-center">Actions</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            orders.map((order, index) => <tr
+                                key={index}
+                            >
+                                <th>{index + 1}</th>
+                                <td>{order.name}</td>
+                                <td>{order.address}</td>
+                                <td>{order.pay}</td>
+                                <td>
+                                    {!order.paid &&
+                                        <Link to={`/dashboard/payment/${order._id}`}>
+                                            <button className='btn btn-xs btn-success'>Pay
+                                            </button>
+                                        </Link>
+                                    }
+
+                                    {order.paid && <div>
+                                        <p><span className="badge badge-success">Paid</span></p>
+                                        {/* <p>Transaction id: <span className='text-success'>{a.transactionId}</span></p> */}
+                                    </div>}
+                                </td>
+                                <td>
+                                    {!order.paid &&
+                                        <button className='btn btn-xs btn-success' onClick={() => handleDelete(order._id)}>Cancel
+                                        </button>
+                                    }
+
+                                    {order.paid && <div>
+                                        <p><span className="badge badge-secondary">Can't Cancel</span></p>
+                                    </div>}
+                                </td>
+                            </tr>)
+                        }
+                    </tbody>
+                </table>
+            </div>
         </div>
     );
 };
