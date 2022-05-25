dcdaaa485d9042a107cbc03e3d105cd957deca44
TanveerShahriar <tanveershahriara@gmail.com>
2022-05-25 17:11:14 +0600

    add review created

--- src/Pages/Dashboard/MyReview.js
+++ src/Pages/Dashboard/MyReview.js
@@ -1,11 +1,101 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import auth from '../../firebase.init';
+import { useForm } from "react-hook-form";
+import { signOut } from 'firebase/auth';
+import { toast } from 'react-toastify';
+import { useNavigate } from 'react-router-dom';
 
-const MyReview = () => {
+const Login = () => {
+    const [user] = useAuthState(auth);
+    const [value, setValue] = useState(5);
+    const { register, formState: { errors }, handleSubmit } = useForm();
+    const navigate = useNavigate()
+
+    const onSubmit = async data => {
+        const name = user.displayName;
+        const description = data.review;
+        const rating = value;
+        const review = { name, description, rating }
+        fetch(`${process.env.REACT_APP_SERVER_LINK}/review`, {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json',
+                authorization: `Bearer ${localStorage.getItem('accessToken')}`
+            },
+            body: JSON.stringify(review)
+        })
+            .then(res => {
+                if (res.status === 401 || res.status === 403) {
+                    signOut(auth);
+                    localStorage.removeItem('accessToken');
+                }
+                return res.json()
+            })
+            .then(data => {
+                toast("Review Added");
+                navigate('/')
+            })
+    }
+
     return (
-        <div>
-            review add
-        </div>
+        <div className='flex justify-center items-center my-12'>
+            <div className="card w-96 bg-primary shadow-xl">
+                <div className="card-body">
+                    <h2 className="text-center text-2xl text-white font-bold">Add Review</h2>
+                    <form onSubmit={handleSubmit(onSubmit)}>
+
+                        <div className="form-control w-full max-w-xs">
+                            <label className="label">
+                                <span className="label-text text-white">Name</span>
+                            </label>
+                            <input
+                                type="text"
+                                value={user.displayName}
+                                readOnly
+                                className="input input-bordered w-full max-w-xs"
+                                {...register("name")}
+                            />
+                        </div>
+                        <div className="form-control w-full max-w-xs">
+                            <label className="label">
+                                <span className="label-text text-white">Review</span>
+                            </label>
+                            <textarea
+                                placeholder="Your review"
+                                className="textarea w-full max-w-xs"
+                                rows={3}
+                                {...register("review", {
+                                    required: {
+                                        value: true,
+                                        message: 'Please add review'
+                                    }
+                                })}
+                            />
+                            <label className="label">
+                                {errors.review?.type === 'required' && <span className="label-text-alt text-yellow-100 text-base">{errors.review.message}</span>}
+                            </label>
+                        </div>
+
+                        <div className="rating">
+                            <input type="radio" name="rating-1" className="mask mask-star" onClick={() => { setValue(1) }} />
+
+                            <input type="radio" name="rating-1" className="mask mask-star" onClick={() => { setValue(2) }} />
+
+                            <input type="radio" name="rating-1" className="mask mask-star" onClick={() => { setValue(3) }} />
+
+                            <input type="radio" name="rating-1" className="mask mask-star" onClick={() => { setValue(4) }} />
+
+                            <input type="radio" name="rating-1" className="mask mask-star" onClick={() => { setValue(5) }} />
+
+                        </div>
+
+                        <input className='btn btn-outline btn-secondary w-full max-w-xs text-white font-bold text-xl' type="submit" value="ADD" />
+                    </form>
+                </div>
+            </div>
+        </div >
     );
 };
 
-export default MyReview;
+export default Login;
--- src/Pages/Home/Review.js
+++ src/Pages/Home/Review.js
@@ -1,15 +1,28 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faStar } from '@fortawesome/free-solid-svg-icons';
 
 const Review = ({ review }) => {
-    const { picture, name, description } = review;
+    const { name, description, rating } = review;
+    const ratings = Array.from(Array(parseInt(rating)).keys());
+    const nonRatings = Array.from(Array(5-parseInt(rating)).keys());
+    useEffect(() => {
+        console.log(ratings);
+        console.log(nonRatings);
+    },[])
     return (
         <div className="card shadow-xl bg-primary">
-            <div className='flex mt-4'>
-                <figure><img className='w-16 h-16 rounded-full mx-8' src={picture} alt="Shoes" /></figure>
-                <h2 className="card-title text-secondary text-4xl">{name}</h2>
-            </div>
+            <h2 className="card-title text-secondary text-4xl ml-7 mt-4">{name}</h2>
             <div className="card-body text-left text-white">
                 <p>{description}</p>
+            </div>
+            <div className='text-left ml-7 flex flex-row mb-4'>
+                {
+                    ratings.map((rate,index) => <p key={index}><FontAwesomeIcon className='text-3xl text-secondary' icon={faStar}/></p>)
+                }
+                {
+                    nonRatings.map((rate,index) => <p key={index}><FontAwesomeIcon className='text-3xl' icon={faStar}/></p>)
+                }
             </div>
         </div>
     );
